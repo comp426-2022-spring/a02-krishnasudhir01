fe6b3754e8e0d28c9130e23933e1c8c43b13ab00
Krishna Sudhir <69811669+krishnasudhir01@users.noreply.github.com>
2022-02-18 22:06:45 -0500

    sub

--- flips.js
+++ flips.js
@@ -1,16 +1,11 @@
 // Require minimist module (make sure you install this one via npm).
 // Use minimist to process one argument `--number=` on the command line after `node flips.js`.
-const min = require('minimist')
 import minimist from 'minimist'
 // Define allowed argument name 'number'.
-//const args = require('minimist')(process.argv.slice(2))
-//const args = process.argv.slice(2)
-var argv = parseArgs(args, opts={})
-console.log(argv);
+const args = minimist(process.argv.slice(2))
 // Define a const `number` using the argument from the command line. 
 args['number']
 const number = args.number || process.env.NUMBER || 1
-console.log(number);
 
 // Import the coinFlip function from your coin.mjs file
 import { coinFlips, countFlips } from './modules/coin.mjs'
