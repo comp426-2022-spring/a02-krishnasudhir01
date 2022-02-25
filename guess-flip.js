2751a507f96c88cff477c708b4563b0bf684402d
Krishna Sudhir <69811669+krishnasudhir01@users.noreply.github.com>
2022-02-24 22:39:59 -0500

    sub4

--- guess-flip.js
+++ guess-flip.js
@@ -7,13 +7,14 @@ const args = minimist(process.argv.slice(2))
 args['call']
 const call = args.call || process.env.CALL
 import { coinFlips, countFlips, flipACoin } from './modules/coin.mjs'
-if (call == ''){
+
+if (!call || call.length == 0) {
 	console.error("Error: no input")
 }
-else if(call != 'heads' && call != 'tails'){
+else if(call != 'heads' && call != 'tails') {
 	console.error("Usage: node guess-flip.js --call=heads|tails")
 }
-else{
+else {
 // Import the coinFlip function from your coin.mjs file
 	console.log(flipACoin(call))
 }
