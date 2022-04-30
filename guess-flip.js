471454bd71ac47b18f78c577aaaecc3d75e50202
Krishna Sudhir <69811669+krishnasudhir01@users.noreply.github.com>
2022-04-29 21:55:49 -0400

    sub8

--- guess-flip.js
+++ guess-flip.js
@@ -1,7 +1,7 @@
 // Require minimist module (make sure you install this one via npm).
 // Use minimist to process one argument `--number=` on the command line after `node flips.js`.
 import minimist from 'minimist';
-import 'module';
+//import 'module';
 
 // Define allowed argument name 'number'.
 const args = require("minimist")(process.argv.slice(2));
@@ -9,20 +9,20 @@ const args = minimist(process.argv.slice(2));
 
 // Define a const `number` using the argument from the command line. 
 args['call']
-const call = args.call; // || process.env.CALL;
+const call = args.call || process.env.CALL;
 import { coinFlips, countFlips, flipACoin } from './modules/coin.mjs';
 
 
 
 if (call ==null){
-	console.error("Error: no input.");
+	console.log("Error: no input.");
 	process.exit();
 }
 /*if (!call || call.length == 0) {
 	console.error("Error: no input")
 }*/
 else if(call != 'heads' && call != 'tails') {
-	console.error("Usage: node guess-flip.js --call=[heads|tails]")
+	console.log("Usage: node guess-flip.js --call=[heads|tails]")
 }
 else {
 // Import the coinFlip function from your coin.mjs file
